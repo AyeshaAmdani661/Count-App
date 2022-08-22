772427e134d9b376319b9aaa6a3ecef76acae277
AyeshaAmdani661 <ayeshaamdani661@gmail.com>
2022-08-22 19:29:34 +0500

    count-app

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+const counter = document.querySelector('#counter');
+const increaseBtn = document.querySelector('.increase');
+const decreaseBtn = document.querySelector('.decrease');
+const reset = document.querySelector('.reset');
+let count = 0;
+increaseBtn.addEventListener("click", () => {
+    count++;
+    counter.innerHTML = count;
+    checkStyle();
+});
+decreaseBtn.addEventListener("click", () => {
+    count--;
+    counter.innerHTML = count;
+    checkStyle();
+});
+checkStyle = () => {
+    if (counter.innerHTML > 0) {
+        counter.style.color = 'green';
+    } else if (counter.innerHTML < 0) {
+        counter.style.color = 'red';
+    } else {
+        counter.style.color = 'white';
+    }
+}
+reset.addEventListener("click", () => {
+    count = 0;
+    counter.innerHTML = count;
+    checkStyle();
+});
