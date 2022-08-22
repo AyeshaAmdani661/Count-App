56f28fbc96effaafa5d9c56652978e59f04be415
AyeshaAmdani661 <ayeshaamdani661@gmail.com>
2022-08-22 19:49:51 +0500

    save changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,27 @@
 const counter = document.querySelector('#counter');
 const increaseBtn = document.querySelector('.increase');
 const decreaseBtn = document.querySelector('.decrease');
-const reset = document.querySelector('.reset');
+const resetBtn = document.querySelector('.reset');
 let count = 0;
+
 increaseBtn.addEventListener("click", () => {
     count++;
     counter.innerHTML = count;
     checkStyle();
 });
+
 decreaseBtn.addEventListener("click", () => {
     count--;
     counter.innerHTML = count;
     checkStyle();
 });
+
+resetBtn.addEventListener("click", () => {
+    count = 0;
+    counter.innerHTML = count;
+    checkStyle();
+});
+
 checkStyle = () => {
     if (counter.innerHTML > 0) {
         counter.style.color = 'green';
@@ -22,8 +31,3 @@ checkStyle = () => {
         counter.style.color = 'white';
     }
 }
-reset.addEventListener("click", () => {
-    count = 0;
-    counter.innerHTML = count;
-    checkStyle();
-});
